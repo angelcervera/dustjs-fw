23a7275ad60b3985ad0feb5b0d552c1d82a14299
angelcervera <angelcervera@simplexportal.com>
2013-01-25 12:28:42 +0100

    Remove unnecessary imports

--- source/com/silyan/dustjs/Main.java
+++ source/com/silyan/dustjs/Main.java
@@ -6,8 +6,6 @@ package com.silyan.dustjs;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 /**
@@ -22,24 +20,6 @@ public class Main {
 	 * @throws ScriptException 
 	 */
 	public static void main(String[] args) {
-		
-		System.out.println("Sacando lista de script engines");
-		for (ScriptEngineFactory sef : new ScriptEngineManager().getEngineFactories()) {
-			  System.out.println(sef);
-			  System.out.append("  Engine: ")
-			      .append(sef.getEngineName())
-			      .append(" ")
-			      .println(sef.getEngineVersion());
-			  System.out.append("  Language: ")
-			      .append(sef.getLanguageName())
-			      .append(" ")
-			      .println(sef.getLanguageVersion());
-			  System.out.append("  Names: ")
-			      .println(sef.getNames());
-			}
-		
-		System.out.println("FIN Sacando lista de script engines");
-		
 		try {
 			new ProjectBuilder( Paths.get(args[0])).process(args[2], Paths.get(args[1]));
 		} catch (Exception e) {
